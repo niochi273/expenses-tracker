f7cc32ba2b435d03966b342478885d89713a733c
niochi273 <valeraveselovsky2000@gmail.com>
2026-07-19 15:50:05 +0700

    extract validations

---
+++ lib/validations.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const categories = [
+  "Housing",
+  "Utilities",
+  "Groceries",
+  "Transportation",
+  "Healthcare",
+  "Dining Out",
+  "Entertainment",
+  "Personal Care",
+  "Clothing",
+  "Gifts",
+  "Debt",
+  "Savings",
+  "Vacation",
+] as const;
+
+export const formSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters.")
+    .max(32, "Title must be at most 32 characters."),
+  description: z
+    .string()
+    .max(100, "Description must be at most 100 characters."),
+  category: z.enum(categories, { message: "Select category" }),
+  amount: z.string().regex(/^\d+$/, {
+    message: "String must contain only numbers",
+  }),
+});
+
+export type Category = (typeof categories)[number];
